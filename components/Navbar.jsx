bc82272c85f7980f3ab162a795f4157d4bf47245
Yunjin <jed.donaire08@gmail.com>
2024-06-22 23:58:34 +0800

    [Build][Jed]- FInalized Navbar  section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
-        <Image src={appleImg} alt="Apple" width={14} height={18}></Image>
+        <Image src={appleImg} alt="Apple" width={14} height={18} className='size-5'></Image>
 
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
@@ -17,8 +17,8 @@ function Navbar() {
         </div>
 
         <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-        <Image src={searchImg} alt="search" width={18} height={18}></Image>
-        <Image src={bagImg} alt="bag" width={18} height={18}></Image>
+        <Image src={searchImg} alt="search" width={18} height={18} className='size-5'></Image>
+        <Image src={bagImg} alt="bag" width={18} height={18} className='size-5'></Image>
         </div>
       </nav>
     </header>
